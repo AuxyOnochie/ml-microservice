009129f918afd02cb40376f221cc22cb223f16e8
AuxyOnochie <107472709+AuxyOnochie@users.noreply.github.com>
2022-09-12 22:55:01 +0000

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath="ebunola/operationalizeml"
+dockerpath="auxyonochie/mlmicroservice"
 
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run operationalizeml\
+kubectl run mlmicroservice\
     --image=$dockerpath\
-    --port=80 --labels app=operationalizeml
+    --port=80 --labels app=mlmicroservice
 
 # Step 3:
 # List kubernetes pods
@@ -20,4 +20,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward operationalizeml 8000:80
+kubectl port-forward mlmicroservice 8000:80
